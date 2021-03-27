3e6e09ee7a8139b26b91ef300f7a66f39d6ceeb9
Rissi <nyxymproxy76@gmail.com>
2021-03-27 13:28:39 +0000

    Add project files.

---
+++ Favourite Number Multiplier/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Favourite_Number_Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int favouritenumberA;
+            int favouritenumberB;
+            int newfavnum;
+            
+            Console.WriteLine("What's your favourite number?");
+            favouritenumberA = int.Parse(Console.ReadLine());
+            Console.WriteLine("What's your second favourite number?");
+            favouritenumberB = int.Parse(Console.ReadLine());
+            newfavnum = favouritenumberA * favouritenumberB;
+            Console.WriteLine("Your new favourite number " + newfavnum);
+        }
+    }
+}
